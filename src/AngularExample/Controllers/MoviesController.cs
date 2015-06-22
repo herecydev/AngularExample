3b2ee8eb0ce4e7e94a5d8a26c27b0f0acfaf7321
herecydev <herecy@live.co.uk>
2015-06-22 15:32:20 +0100

    Added movie model and controller

---
+++ src/AngularExample/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using AngularExample.Models;
+
+namespace AngularExample.Controllers
+{
+	[Route("[movies]")]
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+			return new List<Movie> {
+				new Movie {Id=1, Title="Star Wars", Director="Lucas"},
+				new Movie {Id=2, Title="King Kong", Director="Jackson"},
+				new Movie {Id=3, Title="Memento", Director="Nolan"}
+			};
+		}
+    }
+}
---
+++ src/AngularExample/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace AngularExample.Models
+{
+    public class Movie
+    {
+		public int Id { get; set; }
+
+		public string Title { get; set; }
+
+		public string Director { get; set; }
+	}
+}
