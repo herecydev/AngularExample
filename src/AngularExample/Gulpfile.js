871d54bf935568290db0d4a1e15eef559a0fce72
herecydev <herecy@live.co.uk>
2015-06-22 15:23:07 +0100

    Added bower.js for angular and modified gulp to concat files

--- src/AngularExample/Gulpfile.js
+++ src/AngularExample/Gulpfile.js
@@ -1,15 +1,23 @@
-﻿var gulp = require('gulp');
+﻿/// <binding ProjectOpened='default, watch' />
+var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var del = require('del');
 var scripts = 'Scripts/**/*.js';
 
 gulp.task('compress', function () {
-    return gulp.src(scripts)
+	return gulp.src(scripts)
       .pipe(uglify())
+      .pipe(concat('app.js'))
       .pipe(gulp.dest('wwwroot'));
 });
 
+gulp.task('clean', function () {
+	del(['wwwroot/**.*']);
+})
+
 gulp.task('watch', function () {
-    gulp.watch(scripts, ['compress']);
+	gulp.watch(scripts, ['compress', 'clean']);
 });
 
-gulp.task('default', ['compress']);
+gulp.task('default', ['compress', 'clean']);
