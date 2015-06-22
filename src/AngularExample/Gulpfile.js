6b811e64d75b50885fdc6fa2f9138dbce7cfdfd3
herecydev <herecy@live.co.uk>
2015-06-22 14:18:28 +0100

    Added gulp file

---
+++ src/AngularExample/Gulpfile.js
@@ -0,0 +1,13 @@
+﻿/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+var gulp = require('gulp');
+var uglifier = require('gulp-uglify');
+
+gulp.task('compress', function () {
+    return gulp.src('Scripts/**/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest('wwwroot/app.js'));
+});
