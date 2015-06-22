b617e2b3c991b49e44f35bfd0f32fc793696cc5e
herecydev <herecy@live.co.uk>
2015-06-22 14:55:15 +0100

    Extracted source as it will get replicated.

--- src/AngularExample/Gulpfile.js
+++ src/AngularExample/Gulpfile.js
@@ -1,19 +1,15 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
-var gulp = require('gulp');
-var uglifier = require('gulp-uglify');
+﻿var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var scripts = 'Scripts/**/*.js';
 
 gulp.task('compress', function () {
-    return gulp.src('Scripts/**/*.js')
-      .pipe(uglifier())
-      .pipe(gulp.dest('wwwroot/app.js'));
+    return gulp.src(scripts)
+      .pipe(uglify())
+      .pipe(gulp.dest('wwwroot'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('Scripts/**/*.js', ['compress'])
+    gulp.watch(scripts, ['compress']);
 });
 
-gulp.task('default', ['watch', 'compress']);
+gulp.task('default', ['compress']);
